4548ce8ab78ef6ffba1999d5737d713508348321
jilljurgens <jurgensjill@gmail.com>
2017-10-04 16:47:53 -0500

    working on showing _ for words

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,23 +9,38 @@ function chooseRandomWord (wordArray) {
     var randomWord = wordArray[Math.floor(Math.random() * wordArray.length)];
     console.log(randomWord);
     return randomWord;
-   
+
 }
+   
+
+function blankOutWord(wordToBlank, blankChar){
+	var letterInWord = "";
+    for (var j = 0; j < wordToBlank.length; j++) { // this doesn't work
+	lettersInWord.push("_ ");
+	console.log(wordToBlank);
+}
+}
+//This is to split out the word  like_ _ _ _
+
 var word = chooseRandomWord(words); 
-var lettersInWord = word.split("");
+var lettersInWord = blankOutWord(word);
+//var lettersInWord =[];
 var guessMatches = [];
 var misses = [];
+var lives = [10];
+var x = ("_");
+var y = [];
 
 
-
-
+// document.getElementById("current").innerHTML = guessMatches;
 document.getElementById("guessing").innerHTML = word;
-
+document.getElementById("current").innerHTML = lettersInWord; //why are these showing?
+document.getElementById("guessesleft").innerHTML = lives;
 // for (var i = randomWord.length; i = lettersInWord; i++) {
 // 	guessMatches.push[i];
 // }
 
-//this is good!
+//this is to see if the key pressed matches letters inthe random word
 document.onkeyup = function(event) {
      var key= event.key;
      console.log(key);
@@ -34,6 +49,8 @@ document.onkeyup = function(event) {
 		if (key == word[i]) {
 			inWord = true;
 			guessMatches[i] = word[i];
+//			word.push(guessMatches);//how do I add to array each time? Just leaving 1 letter
+
 
 		}
 
@@ -42,14 +59,13 @@ document.onkeyup = function(event) {
 	if (inWord) {
 		misses.push(key);
 		document.getElementById("guessedletters").innerHTML = (key);
+//		lives--; //added this line to update lives left
+//		document.getElementById("guessesLeft").innerHTML = (lives);
 	}
 }
 
 
 //now put this in a string.	
-
-
-
 
 
 // append multiple values to the array
@@ -73,6 +89,6 @@ document.onkeyup = function(event) {
 //check if there are any__ left to guess in the word
 //add counter for number of wins
 
-//generate _ _ on the screen for the lenght of word
+//generate _ _ on the screen for the length of word
 
 
